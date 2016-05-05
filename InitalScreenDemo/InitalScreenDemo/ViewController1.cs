c6dc833618cd93ed58a9737e62250cef8b230575
PK <pranav@ciedigital.com>
2016-05-05 13:50:35 -0700

    initial commit

---
+++ InitalScreenDemo/InitalScreenDemo/ViewController1.cs
@@ -0,0 +1,20 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace InitalScreenDemo
+{
+	partial class ViewController1 : UIViewController
+	{
+		public ViewController1(IntPtr handle)
+			: base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+
+		}
+	}
+}
---
+++ InitalScreenDemo/InitalScreenDemo/obj/Xcode/1/ViewController1.h
@@ -0,0 +1,16 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio Business to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController1 : UIViewController {
+
+}
+
+@end
